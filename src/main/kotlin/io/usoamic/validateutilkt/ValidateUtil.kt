7011135cd0c88b5673b83e6fde345297d32d9982
primys <a2uso@tuta.io>
2020-09-26 17:48:17 -0700

    Replace class to object and annotate all methods as @JvmStatic

--- src/main/kotlin/io/usoamic/validateutilkt/ValidateUtil.kt
+++ src/main/kotlin/io/usoamic/validateutilkt/ValidateUtil.kt
@@ -6,125 +6,137 @@ import org.web3j.crypto.WalletUtils
 import java.math.BigDecimal
 import java.math.BigInteger
 
-class ValidateUtil {
-    companion object {
-        fun validatePassword(password: String) = apply {
-            validateThatNotEmpty(
-                password,
-                EmptyPasswordError()
-            )
+object ValidateUtil {
+    @JvmStatic
+    fun validatePassword(password: String) = apply {
+        validateThatNotEmpty(
+            password,
+            EmptyPasswordError()
+        )
+    }
+
+    @JvmStatic
+    fun validatePasswords(password: String, confirmPassword: String) {
+        validateThatNotEmpty(
+            password,
+            EmptyPasswordError()
+        )
+
+        validateThatNotEmpty(
+            confirmPassword,
+            EmptyConfirmPasswordError()
+        )
+
+        if (password != confirmPassword) {
+            throw PasswordsDoNotMatchError()
         }
+    }
 
-        fun validatePasswords(password: String, confirmPassword: String) {
-            validateThatNotEmpty(
-                password,
-                EmptyPasswordError()
-            )
+    @JvmStatic
+    fun validateDescription(description: String) = apply {
+        validateThatNotEmpty(
+            description,
+            EmptyDescriptionError()
+        )
+    }
 
-            validateThatNotEmpty(
-                confirmPassword,
-                EmptyConfirmPasswordError()
-            )
+    @JvmStatic
+    fun validateNoteContent(content: String) = apply {
+        validateThatNotEmpty(
+            content,
+            EmptyNoteContentError()
+        )
+    }
 
-            if(password != confirmPassword) {
-                throw PasswordsDoNotMatchError()
-            }
+    @JvmStatic
+    fun validateComment(comment: String) = apply {
+        validateThatNotEmpty(
+            comment,
+            EmptyCommentError()
+        )
+    }
+
+    @JvmStatic
+    fun validatePrivateKey(privateKey: String) = apply {
+        validateThatNotEmpty(
+            privateKey,
+            EmptyPrivateKeyError()
+        )
+        if (!WalletUtils.isValidPrivateKey(privateKey)) {
+            throw InvalidPrivateKeyError()
         }
+    }
 
-        fun validateDescription(description: String) = apply {
-            validateThatNotEmpty(
-                description,
-                EmptyDescriptionError()
-            )
+    @JvmStatic
+    fun validateMnemonicPhrase(mnemonicPhrase: String) = apply {
+        validateThatNotEmpty(
+            mnemonicPhrase,
+            EmptyMnemonicPhraseError()
+        )
+        if (!MnemonicUtils.validateMnemonic(mnemonicPhrase)) {
+            throw InvalidMnemonicPhraseError()
+
         }
+    }
 
-        fun validateNoteContent(content: String) = apply {
-            validateThatNotEmpty(
-                content,
-                EmptyNoteContentError()
-            )
+    @JvmStatic
+    fun validateAddress(address: String) = apply {
+        validateThatNotEmpty(
+            address,
+            EmptyAddressError()
+        )
+        if (!WalletUtils.isValidAddress(address)) {
+            throw InvalidAddressError()
         }
+    }
 
-        fun validateComment(comment: String) = apply {
-            validateThatNotEmpty(
-                comment,
-                EmptyCommentError()
-            )
+    @JvmStatic
+    fun validateTransferValue(value: String) = apply {
+        validateThatNotEmpty(
+            value,
+            EmptyValueError()
+        )
+        val decimalVal = value.toBigDecimalOrNull() ?: throw InvalidValueError()
+        if (decimalVal <= BigDecimal.ZERO) {
+            throw InvalidValueError()
         }
+    }
 
-        fun validatePrivateKey(privateKey: String) = apply {
-            validateThatNotEmpty(
-                privateKey,
-                EmptyPrivateKeyError()
-            )
-            if (!WalletUtils.isValidPrivateKey(privateKey)) {
-                throw InvalidPrivateKeyError()
-            }
+    @JvmStatic
+    fun validateAppId(appId: String) = apply {
+        validateThatNotEmpty(
+            appId,
+            EmptyAppIdError()
+        )
+    }
+
+    @JvmStatic
+    fun validatePurchaseId(purchaseId: String) = apply {
+        validateThatNotEmpty(
+            purchaseId,
+            EmptyPurchaseIdError()
+        )
+    }
+
+    @JvmStatic
+    fun validateId(id: String) = apply {
+        val intId = id.toBigIntegerOrNull() ?: throw InvalidIdError()
+        if (intId < BigInteger.ZERO) {
+            throw InvalidIdError()
         }
+    }
 
-        fun validateMnemonicPhrase(mnemonicPhrase: String) = apply {
-            validateThatNotEmpty(
-                mnemonicPhrase,
-                EmptyMnemonicPhraseError()
-            )
-            if (!MnemonicUtils.validateMnemonic(mnemonicPhrase)) {
-                throw InvalidMnemonicPhraseError()
-
-            }
+    @JvmStatic
+    fun validateIds(vararg ids: String) = apply {
+        for (id in ids) {
+            validateId(id)
         }
+    }
 
-        fun validateAddress(address: String) = apply {
-            validateThatNotEmpty(
-                address,
-                EmptyAddressError()
-            )
-            if (!WalletUtils.isValidAddress(address)) {
-                throw InvalidAddressError()
-            }
-        }
-
-        fun validateTransferValue(value: String) = apply {
-            validateThatNotEmpty(
-                value,
-                EmptyValueError()
-            )
-            val decimalVal = value.toBigDecimalOrNull() ?: throw InvalidValueError()
-            if (decimalVal <= BigDecimal.ZERO) {
-                throw InvalidValueError()
-            }
-        }
-
-        fun validateAppId(appId: String) = apply {
-            validateThatNotEmpty(
-                appId,
-                EmptyAppIdError()
-            )
-        }
-
-        fun validatePurchaseId(purchaseId: String) = apply {
-            validateThatNotEmpty(
-                purchaseId,
-                EmptyPurchaseIdError()
-            )
-        }
-
-        fun validateId(id: String) = apply {
-            val intId = id.toBigIntegerOrNull() ?: throw InvalidIdError()
-            if (intId < BigInteger.ZERO) {
-                throw InvalidIdError()
-            }
-        }
-
-        fun validateIds(vararg ids: String) = apply {
-            for (id in ids) {
-                validateId(id)
-            }
-        }
-
-        private fun validateThatNotEmpty(str: String, e: ValidateUtilError) {
-            if (str.isEmpty()) {
-                throw e
-            }
+    @JvmStatic
+    private fun validateThatNotEmpty(str: String, e: ValidateUtilError) {
+        if (str.isEmpty()) {
+            throw e
         }
     }
 }
